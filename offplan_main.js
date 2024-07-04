47e8911973079ff6b9776f7ebc44bda604b2f20f
shamim-s <shamimibas@gmail.com>
2024-07-04 23:10:04 +0600

    removed optional chaining

--- offplan_main.js
+++ offplan_main.js
@@ -215,7 +215,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 let propertySelectField = document.querySelector(".developer-select");
 let inputField = document.querySelector(".developer-filter");
-propertySelectField?.addEventListener("change", function () {
+propertySelectField.addEventListener("change", function () {
   inputField.value = propertySelectField.value;
   console.log(inputField.value);
   const inputEvent = new Event("input", {
@@ -226,7 +226,7 @@ propertySelectField?.addEventListener("change", function () {
 });
 let propertySelectField1 = document.querySelector(".custom-are-select");
 let inputField1 = document.querySelector(".custom-area-search");
-propertySelectField1?.addEventListener("change", function () {
+propertySelectField1.addEventListener("change", function () {
   inputField1.value = propertySelectField1.value;
   console.log(inputField1.value);
   const inputEvent = new Event("input", {
